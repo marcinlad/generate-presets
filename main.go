7e662cf112043b3eef42a0505523981d921400ad
Marcin Ładniak <marcinladniak2@gmail.com>
2024-07-23 13:30:24 +0200

    implement retry

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ import (
 )
 
 var (
-	authHash    string
-	table       string
-	endpointUrl string
+	authHash          string
+	table             string
+	endpointUrl       string
+	allProcessedItems = 0
 )
 
 func main() {
@@ -62,7 +63,7 @@ func scan(client *dynamodb.Client) {
 		fmt.Println("Time taken to generate presets: ", time.Since(start).Milliseconds(), "ms")
 
 		if result.LastEvaluatedKey == nil {
-			fmt.Println("Ending. Scanned items: ", scannedItems)
+			fmt.Println("Ending. Scanned items: ", scannedItems, "Processed items: ", allProcessedItems)
 			break
 		}
 
@@ -87,14 +88,31 @@ func generatePresets(items []map[string]types.AttributeValue) {
 				return
 			}
 
-			res, err := fetch(pictures)
-			if err != nil {
-				log.Println("fetch failed", err)
-				return
-			}
-			defer res.Body.Close()
+			retryLimit := 3
 
-			log.Println("Response status: ", res.Status)
+			for i := 0; i < retryLimit; i++ {
+				res, err := fetch(pictures)
+				if err != nil {
+					log.Println("fetch failed", err)
+					return
+				}
+				defer res.Body.Close()
+
+				if res.StatusCode == 200 {
+					fmt.Println("Batch processed successfully")
+					break
+				}
+
+				if i == retryLimit-1 {
+					log.Println("Batch failed to process after 3 retries")
+					return
+				}
+
+				log.Println("Response status: ", res.Status)
+				fmt.Println("Retrying...")
+
+				time.Sleep(500 * time.Millisecond)
+			}
 
 			processedItems += len(batch)
 		}(batch)
@@ -104,4 +122,6 @@ func generatePresets(items []map[string]types.AttributeValue) {
 
 	fmt.Println("All batches processed")
 	fmt.Println("Processed items: ", processedItems)
+
+	allProcessedItems += processedItems
 }
